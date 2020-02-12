e731f55583808e446f65420921d5f974e4ea41f2
tino queija <martin.queija@wolox.com.ar>
2020-02-12 16:01:46 -0300

    Create book repository interface extending JpaRepository

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,8 +1,8 @@
 package wolox.training.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import wolox.training.models.Book;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
     Book findByTitle(String title);
 }
