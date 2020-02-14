5aefa80a51ea6a0c807329b436db48739ec4c88b
tino queija <martin.queija@wolox.com.ar>
2020-02-14 11:15:45 -0300

    Set correclty base packages to scan for springboot, jpaRepositories and entity scan.

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.baeldung.persistence.repo")
-@EntityScan("com.baeldung.persistence.model")
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"wolox.training"})
+@EnableJpaRepositories(basePackages = {"wolox.training.repositories"})
+@EntityScan(basePackages =  {"wolox.training.models"})
 public class TrainingApplication {
 
 	public static void main(String[] args) {
