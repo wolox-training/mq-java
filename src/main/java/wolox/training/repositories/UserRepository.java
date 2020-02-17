2f1e3509eb948fc697120af7148e8e36f721ce14
tino queija <martin.queija@wolox.com.ar>
2020-02-17 11:39:05 -0300

    create user repository with findByUsername method

---
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package wolox.training.repositories;
+
+import java.util.Optional;
+import javax.validation.constraints.NotNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import wolox.training.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(@NotNull String title);
+}
