bddb9bcfecf2a966295ddc26bd0aab4a691b0c46
tino queija <martin.queija@wolox.com.ar>
2020-02-17 11:47:30 -0300

    remove @entityScan and @enableJpaRepositories

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -2,11 +2,7 @@ package wolox.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.baeldung.persistence.repo")
-@EntityScan("com.baeldung.persistence.model")
 @SpringBootApplication
 public class TrainingApplication {
 
